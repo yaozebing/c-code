5f3840db0dbe3cee60a82e3506ed98d89d2faf0b
姚泽冰 <1102898227@qq.com>
2020-02-28 16:05:10 +0800

    1

--- 2_26/2_26/2_26.c
+++ 2_26/2_26/2_26.c
@@ -11,35 +11,71 @@
 //	return 0;
 //}
 
-#include<stdio.h>
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0, b = 0, c = 0, d = 0, e = 0;
+//	for (a = 0; a < 5; a++)
+//	{
+//		for (b = 1; b <= 5; b++)
+//		{
+//			for (c = 1; c <= 5; c++)
+//			{
+//				for (d = 1; d <= 5; d++)
+//				{
+//					for (e = 1; e <= 5; e++)
+//					{
+//						if (((b == 2) + (a == 3)) == 1 && 
+//							((b == 2) + (e == 4)) == 1 && 
+//							((c == 1) + (d == 2)) == 1 && 
+//							((c == 5) + (d == 3)) == 1 && 
+//							((e == 4) + (a == 1) == 1))
+//						{
+//							if (120 == (a*b*c*d*e))
+//							{
+//								printf("%d %d %d %d %d ", a, b, c, d, e );
+//							}
+//						}
+//					}
+//				}
+//			}
+//		}
+//	}
+//	return 0;
+//}
+
+//输出什么？
+//#include <stdio.h>
+//int main()
+//{
+//	char a = -1;
+//	signed char b = -1;
+//	unsigned char c = -1;
+//	printf("a=%d,b=%d,c=%d", a, b, c);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	char a = -128;
+//	printf("%u\n", a);
+//	return 0;
+//}
+
+//unsigned int i;
+//for (i = 9; i >= 0; i--)
+//{
+//	printf("%u\n", i);
+//}
+//1.
+////输出什么？
+#include <stdio.h>
 int main()
 {
-	int a = 0, b = 0, c = 0, d = 0, e = 0;
-	for (a = 0; a < 5; a++)
-	{
-		for (b = 1; b <= 5; b++)
-		{
-			for (c = 1; c <= 5; c++)
-			{
-				for (d = 1; d <= 5; d++)
-				{
-					for (e = 1; e <= 5; e++)
-					{
-						if (((b == 2) + (a == 3)) == 1 && 
-							((b == 2) + (e == 4)) == 1 && 
-							((c == 1) + (d == 2)) == 1 && 
-							((c == 5) + (d == 3)) == 1 && 
-							((e == 4) + (a == 1) == 1))
-						{
-							if (120 == (a*b*c*d*e))
-							{
-								printf("%d %d %d %d %d ", a, b, c, d, e );
-							}
-						}
-					}
-				}
-			}
-		}
-	}
+	char a = -1;
+	signed char b = -1;
+	unsigned char c = -1;
+	printf("a=%d,b=%d,c=%d", a, b, c);
 	return 0;
 }
