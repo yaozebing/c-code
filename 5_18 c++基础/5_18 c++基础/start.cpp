db0705302a80c9359ca231fb635a0e48655888bf
姚泽冰 <1102898227@qq.com>
2020-05-18 23:37:53 +0800

    的

---
+++ 5_18 c++基础/5_18 c++基础/start.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+namespace bite
+{
+	void print(int a) {
+		printf("%d\n", a);
+	}
+
+	int a;
+	int b;
+	namespace bite_79 {
+		void fun1
+	}
+}
+int main() {
+
+}
